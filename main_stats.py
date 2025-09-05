a99b2f76d513e520ac9d10363ff5edc04b1210b0
frp09 <fr.papi@unifi.it>
2025-09-05 18:30:50 +0200

    added possibility to plot aerodynamic spanwise quantites

--- main_stats.py
+++ main_stats.py
@@ -1,57 +1,145 @@
 import matplotlib.pyplot as plt
-from mappings import qblade_to_openfast_map
-from data_utils import load_and_map_stats
-from plotting import plot_stat_vs_wind, add_shared_legend, plot_spanwise_comparison
+from mappings import qblade_to_openfast_map, qblade_nodes_map
+from data_utils import load_and_map_stats, load_DELs
+from plotting import plot_stat_vs_wind, add_shared_legend, plot_spanwise_comparison, plot_DEL_vs_wind, plot_spanwise_aero
 from matplotlib.colors import LinearSegmentedColormap
 import numpy as np
 
+
+#%% USER INPUTS ------------------------------------------------------------------------
+
 ss_names = [
 
-    # r'/home/papi/FLOATFARM/QB-WEIS-COUPLE/NEW_MED_OPTIMIZATION/MED15-308_v20.1.0/mout_MED15-300_v20.0.0_OPT/iteration_0/summary_stats.p',
-    # r'/home/papi/FLOATFARM/QB-WEIS-COUPLE/NEW_MED_OPTIMIZATION/MED15-308_v20.1.0/mout_MED15-300_v20.0.0_OPT/iteration_46/summary_stats.p',
-    # r'/home/papi/FLOATFARM/QB-WEIS-COUPLE/NEW_MED_OPTIMIZATION/MED15-308_v20.1.0_OPT_II/mout_MED15-308_v20.3.0_OPT/iteration_0/summary_stats.p'
-    r'/home/papi/FLOATFARM/QB-WEIS-COUPLE/NEW_MED_OPTIMIZATION/MED15-308_v20.1.0_OPT_III/mout_MED15-308_v20.3.0_OPT/iteration_0/summary_stats.p',
-    # r'/home/papi/FLOATFARM/QB-WEIS-COUPLE/NEW_MED_OPTIMIZATION/MED15-308_v20.1.0_OPT_III/mout_MED15-308_v20.3.0_OPT/iteration_120/summary_stats.p',
-    # r'/home/papi/FLOATFARM/QB-WEIS-COUPLE/NEW_MED_OPTIMIZATION/MED15-308_v20.1.0_OPT_III/mout_MED15-308_v20.3.0_OPT/iteration_121/summary_stats.p',
-    # r'/home/papi/FLOATFARM/QB-WEIS-COUPLE/NEW_MED_OPTIMIZATION/MED15-308_v20.1.0_OPT_III/mout_MED15-308_v20.3.0_OPT/iteration_122/summary_stats.p',
-    # r'/home/papi/FLOATFARM/QB-WEIS-COUPLE/NEW_MED_OPTIMIZATION/MED15-308_v20.1.0_OPT_III/mout_MED15-308_v20.3.0_OPT/iteration_123/summary_stats.p',
-    # r'/home/papi/FLOATFARM/QB-WEIS-COUPLE/NEW_MED_OPTIMIZATION/MED15-308_v20.1.0_OPT_III/mout_MED15-308_v20.3.0_OPT/iteration_124/summary_stats.p',
-    # r'/home/papi/FLOATFARM/QB-WEIS-COUPLE/NEW_MED_OPTIMIZATION/MED15-308_v20.1.0_OPT_III/mout_MED15-308_v20.3.0_OPT/iteration_125/summary_stats.p',
-    # r'/home/papi/FLOATFARM/QB-WEIS-COUPLE/NEW_MED_OPTIMIZATION/MED15-308_v20.1.0_OPT_III/mout_MED15-308_v20.3.0_OPT/iteration_126/summary_stats.p',
-    # r'/home/papi/FLOATFARM/QB-WEIS-COUPLE/NEW_MED_OPTIMIZATION/MED15-308_v20.1.0_OPT_III/mout_MED15-308_v20.3.0_OPT/iteration_127/summary_stats.p',
-    r'/home/papi/FLOATFARM/QB-WEIS-COUPLE/NEW_MED_OPTIMIZATION/MED15-308_v20.1.0_OPT_III/mout_MED15-308_v20.3.0/iteration_0/summary_stats.p',
-    r'/home/papi/FLOATFARM/QB-WEIS-COUPLE/NEW_MED_OPTIMIZATION/MED15-308_v20.4.0/mout_MED15-308_v20.4.0/iteration_0/summary_stats.p',
+    r'/home/papi/FLOATFARM/QB-WEIS-COUPLE/NEW_MED_OPTIMIZATION/COMPARE_SPAR_POS/MED15-308_v20.6.0_OPT/R1/mout_MED15-308_v20.7.0/iteration_0/summary_stats.p',
+    r'/home/papi/FLOATFARM/QB-WEIS-COUPLE/NEW_MED_OPTIMIZATION/COMPARE_SPAR_POS/MED15-308_v20.6.0_OPT/R1/mout_MED15-308_v20.7.0/iteration_318/summary_stats.p',
+    r'/home/papi/FLOATFARM/QB-WEIS-COUPLE/NEW_MED_OPTIMIZATION/COMPARE_SPAR_POS/MED15-308_v20.6.1_OPT/R1/mout_MED15-308_v20.7.1/iteration_324/summary_stats.p',
+    r'/home/papi/FLOATFARM/QB-WEIS-COUPLE/NEW_MED_OPTIMIZATION/COMPARE_SPAR_POS/MED15-308_v20.6.2_OPT/R1/mout_MED15-308_v20.7.2/iteration_329/summary_stats.p',
+]
+
+#ss_names = [
+#    r'/home/papi/FLOATFARM/QB-WEIS-COUPLE/NEW_MED_OPTIMIZATION/MED15-308_v20.6.1_OPT/R1/mout_MED15-308_v20.7.1/iteration_0/summary_stats.p',
+#    #r'/home/papi/FLOATFARM/QB-WEIS-COUPLE/NEW_MED_OPTIMIZATION/MED15-308_v20.6.1_OPT/R1/mout_MED15-308_v20.7.1/iteration_324/summary_stats.p',
+#    r'/home/papi/FLOATFARM/QB-WEIS-COUPLE/NEW_MED_OPTIMIZATION/MED15-308_v20.6.1_OPT/R2/mout_MED15-308_v20.7.1/iteration_159/summary_stats.p',
+#    r'/home/papi/FLOATFARM/QB-WEIS-COUPLE/NEW_MED_OPTIMIZATION/MED15-308_v20.6.1_OPT/R3/iteration_0/summary_stats.p',
+#
+#]
+
+lables = [
+
+        'start',
+        'middle', 
+        'forward', 
+        'back'
+          
+          ]
+
+
+#ss_names = [
+#    r'/home/papi/FLOATFARM/QB-WEIS-COUPLE/00_QBTOWEIS_VERIFICATION_IEA22/VALIDATE_RADIANS_DEGREES/mout_SONATA_deg/iteration_0/summary_stats.p',
+#    r'/home/papi/FLOATFARM/QB-WEIS-COUPLE/00_QBTOWEIS_VERIFICATION_IEA22/VALIDATE_RADIANS_DEGREES/mout_SONATA_rad/iteration_0/summary_stats.p',
+#]
+#
+#lables = [
+#
+#        'deg',
+#        'red', 
+#          
+#          ]
+#
+#ss_names = [
+#    r'/home/papi/FLOATFARM/QB-WEIS-COUPLE/NEW_MED_OPTIMIZATION/MED15-308_v20.6.1_OPT/R2/iteration_0/summary_stats.p',
+#    r'/home/papi/FLOATFARM/QB-WEIS-COUPLE/NEW_MED_OPTIMIZATION/MED15-308_v31.1.0_OPT/iteration_0/summary_stats.p',
+#]
+#
+#lables = [
+#
+#        '2 shear webs',
+#        '3 shear webs', 
+#          
+#          ]
+
+#ss_names = [
+#    #r'/home/papi/FLOATFARM/QB-WEIS-COUPLE/NEW_MED_OPTIMIZATION/MED15-308_v20.6.1_OPT/R1/mout_MED15-308_v20.7.1/iteration_0/summary_stats.p',
+#    r'/home/papi/FLOATFARM/QB-WEIS-COUPLE/NEW_MED_OPTIMIZATION/COMPARE_SPAR_POS/MED15-308_v20.6.1_RUN/mout_MED15-308_v20.6.1_LD1/iteration_0/summary_stats.p',
+#    r'/home/papi/FLOATFARM/QB-WEIS-COUPLE/NEW_MED_OPTIMIZATION/COMPARE_SPAR_POS/MED15-308_v20.6.1_RUN/mout_MED15-308_v20.6.1_LD2/iteration_0/summary_stats.p',
+#    r'/home/papi/FLOATFARM/QB-WEIS-COUPLE/NEW_MED_OPTIMIZATION/COMPARE_SPAR_POS/MED15-308_v20.6.1_RUN/mout_MED15-308_v20.6.1_LD3/iteration_0/summary_stats.p',
+#
+#]
+#
+#lables = [
+#        #'1-2%% crit',
+#        '0.5-1% crit',
+#        '0.25-0.5%crit', 
+#        '0%c crit'
+#          
+#          ]
+#
+# ---------------------------------------------------------------------------------------
+
+
+#ss_names = [
+#    r'/home/papi/FLOATFARM/QB-WEIS-COUPLE/NEW_MED_OPTIMIZATION/MED15-308_v31.1.0_OPT_II/mout_MED15-308_v31.1.2/iteration_0/summary_stats.p',
+#    r'/home/papi/FLOATFARM/QB-WEIS-COUPLE/NEW_MED_OPTIMIZATION/MED15-308_v31.1.0_OPT_III/mout_MED15-308_v31.1.3/iteration_0/summary_stats.p',
+#
+#]
+#
+#lables = [
+#
+#        '31.1.0_II_it0',
+#        '31.1.0_III_it0', 
+#          
+#          ]
+
+# ------------------------------------------------------------------------------------------
+ss_names = [
+
+    r'/home/papi/FLOATFARM/QB-WEIS-COUPLE/NEW_MED_OPTIMIZATION/MED15-308_v30.1.0_CtrlOpt/R1/mout_MED15-308_v30.1.1/iteration_0/summary_stats.p',
+    #r'/home/papi/FLOATFARM/QB-WEIS-COUPLE/NEW_MED_OPTIMIZATION/MED15-308_v30.1.0_CtrlOpt/mout_MED15-308_v30.1.1/iteration_84/summary_stats.p',
 
 ]
 
 lables = [
-          'v20.3.0_ITER1',
-          # 'v20.3.0_ITER120',
-          # 'v20.3.0_ITER121',
-          # 'v20.3.0_ITER122',
-          # 'v20.3.0_ITER123',
-          # 'v20.3.0_ITER124',
-          # 'v20.3.0_ITER125',
-          # 'v20.3.0_ITER126',
-          # 'v20.3.0_ITER127',
-          'v20.3.0_lowdamp',
-          'v20.4.0_ITER0',
+
+        'iter_0', 
+        'iter_84'
           
           ]
+
+savewind = False
+savespan = False
+outn_wnd = r'/home/papi/FLOATFARM/QB-WEIS-COUPLE/NEW_MED_OPTIMIZATION/MED15-308_v20.6.1_OPT/R2/compare_wind_R1R2.svg'
+outn_spn = r'/home/papi/FLOATFARM/QB-WEIS-COUPLE/NEW_MED_OPTIMIZATION/MED15-308_v20.6.1_OPT/R2/compare_span_R1R2_7ms.svg'
+
+# Plotting: Spanwise comparisons
+sim_index_to_plot = 10
+
+
 
 title = ['(a)', '(b)', '(c)', '(d)', '(e)', '(f)']
 
 channels = ['BldPitch1',
             'RtAeroCp',
             # 'RtAeroCt',
-            # 'GenPwr', 
+            #'GenPwr', 
             # 'GenTq',
-            'RotSpeed',
+            #'RotSpeed',
             'RtAeroTh',
-            'TipDxc1',
-            #'RDzb1_0.800',
+            #'TipDxc1',
+            'TipDyc1',
+            'RDzb1_1.000',
             'RootMyc1'
             ]
+
+DELchannels = [
+    'X_b RootBend. Mom. BLD_1',
+    'Y_b RootBend. Mom. BLD_2',
+    'Z_b RootBend. Mom. BLD_3',
+    'RootMc1', 
+    'RootMc2', 
+    'RootMc3'
+
+
+]
 
 label_map = {
     'BldPitch1': 'Blade pitch (°)',
@@ -66,15 +154,23 @@ label_map = {
 
 cmap_custom_green = LinearSegmentedColormap.from_list(
     "gray_to_teal", 
-    ["#FAAA0A", "#006BA6"]  # Warm gray to teal
+    #["#FAAA0A", "#006BA6"]  # Warm gray to teal
+    [ "#A9A9A9", 
+     '#e05252', 
+     "royalblue", 
+     '#4ca64c'
+     ]
 )
 
 colors = cmap_custom_green(np.linspace(0, 1, len(ss_names)))
 linestyles = ['-.', '--', ':', '-', '-.', '--', ':', '-']
 
 # Load data
-stats_raw, stats = load_and_map_stats(ss_names, qblade_to_openfast_map, drop_unmapped=True, verbose=True)
+stats_raw, stats = load_and_map_stats(ss_names, qblade_to_openfast_map, drop_unmapped=True, verbose=False)
+del_paths = [ss_names[i].replace('summary_stats.p', 'DELs.p') for i in range(len(ss_names))]
+DELs = load_DELs(del_paths)
 
+# %%---------------------------------------------------------------------------#
 # Plotting: mean/min/max as function of wind speed 
 fig, axs = plt.subplots(2, 3, figsize=(10, 7))
 axs = axs.flatten()
@@ -95,15 +191,65 @@ for i, chan in enumerate(channels):
 
 handles, labels = axs[0].get_legend_handles_labels()
 add_shared_legend(fig, handles, labels)
+plt.show()
 
+if savewind: 
+    fig.savefig(outn_wnd, dpi = 300)
 
-# Plotting: Spanwise comparisons
-sim_index_to_plot = 15
+# ---------------------------------------------------------------------------#
+# Plotting: DELs as function of wind speed 
+fig, axs = plt.subplots(2, 3, figsize=(10, 7))
+axs = axs.flatten()
+
+for i, chan in enumerate(DELchannels):
+    for j in range(len(stats)):
+        
+        plot_DEL_vs_wind(            
+            axs[i],
+            stats[j], 
+            DELs[j], 
+            x_col='Wind1VelX',
+            y_col=chan,
+            style={'color': colors[j], 'linestyle': '', 'marker': '.'},
+            label=lables[j]
+            )
+        
+    axs[i].set_title(title[i], fontsize='large')
+    axs[i].set_ylabel(label_map.get(chan, chan))
+    axs[i].set_xlabel("Mean Wind Speed (m/s)")
+
+handles, labels = axs[0].get_legend_handles_labels()
+add_shared_legend(fig, handles, labels)
+plt.show()
+
+# ---------------------------------------------------------------------------#
+# Plotting: qblade struct spanwise quantities
 
 spanwise_channels = [    
           'RDzb1',
-          'AeroFyb1',  
-          'AeroFxb1',
+          #'AeroFyb1',  
+          #'AeroFxb1',
+          'B1Alpha',
+          #'B1AxInd',
+          'TDyb1',
           'TDxb1']
 
-plot_spanwise_comparison(stats, spanwise_channels, lables, colors, linestyles, sim_label=sim_index_to_plot, title_prefix="Spanwise: ", show_extrema=True)
+fig, axs = plt.subplots(1, len(spanwise_channels), figsize=(3.5 * len(spanwise_channels), 4))
+plot_spanwise_comparison(stats, spanwise_channels, lables, colors, linestyles, fig, axs, sim_label=sim_index_to_plot, title_prefix="Spanwise: ", show_extrema=True)
+plt.show()
+
+if savespan:
+    fig.savefig(outn_spn, dpi = 300)
+
+# ---------------------------------------------------------------------------#
+# Plotting: qblade aero  spanwise quantites such as Cl, Cd, Induction
+
+spanwise_aero_channels = [    
+          'B1AxInd', 
+          'B1Cl',
+          'B1Cd',]
+
+fig, axs = plt.subplots(1, len(spanwise_aero_channels), figsize=(3.5 * len(spanwise_aero_channels), 4))
+plot_spanwise_aero(stats, spanwise_aero_channels, qblade_nodes_map, labels, colors, linestyles, fig, axs, sim_label=sim_index_to_plot, title_prefix="", show_extrema=True)
+plt.show()
+
--- plotting.py
+++ plotting.py
@@ -11,12 +11,19 @@ def plot_stat_vs_wind(ax, data, x_col, y_col, style, label):
     y_min = data[y_col]['min']
 
     ax.plot(x, y_mean, label=label, **style)
-    ax.scatter(x, y_max, edgecolor=style['color'], color='none', marker='^')
-    ax.scatter(x, y_min, edgecolor=style['color'], color='none', marker='v')
+    ax.scatter(x, y_max, edgecolor=style['color'], color='none', marker='^', s = 14)
+    ax.scatter(x, y_min, edgecolor=style['color'], color='none', marker='v', s = 14)
     ax.grid(linestyle =':', color = 'gray')
 
-def plot_spanwise_comparison(stats_mapped, spanwise_channels, labels, colors, linestyles, sim_label, title_prefix="", show_extrema=False):
-    fig, axs = plt.subplots(1, len(spanwise_channels), figsize=(3.5 * len(spanwise_channels), 4))
+def plot_DEL_vs_wind(ax, data_st, data_DEL, x_col, y_col, style, label):
+    x = data_st[x_col]['mean']
+    y_mean = data_DEL[y_col]
+
+    ax.plot(x, y_mean, label=label, **style)
+    ax.grid(linestyle =':', color = 'gray')
+
+def plot_spanwise_comparison(stats_mapped, spanwise_channels, labels, colors, linestyles, fig, axs, sim_label, title_prefix="", show_extrema=False):
+    
     if len(spanwise_channels) == 1:
         axs = [axs]
 
@@ -79,8 +86,72 @@ def plot_spanwise_comparison(stats_mapped, spanwise_channels, labels, colors, li
 
     handles, labels = axs[0].get_legend_handles_labels()
     add_shared_legend(fig, handles, labels)
-    plt.show()
 
+def plot_spanwise_aero(stats_mapped, spanwise_channels, span_map, labels, colors, linestyles, fig, axs, sim_label, title_prefix="", show_extrema=False):
+    
+    if len(spanwise_channels) == 1:
+        axs = [axs]
+
+    for i, chan in enumerate(spanwise_channels):
+        ax = axs[i]
+
+        for j, df in stats_mapped.items():
+            try:
+                # Extract mean, max and min slices from multi-index DataFrame
+                df_mean = df.xs('mean', axis=1, level=1)
+                df_max = df.xs('max', axis=1, level=1) if show_extrema else None
+                df_min = df.xs('min', axis=1, level=1) if show_extrema else None
+
+                # Construct row label from simulation index, zero-padded
+                row_label = str(sim_label).zfill(2)
+                lbl = False
+                for elm in df_mean.index:
+                    if '_'+row_label+'_' in elm: 
+                        lbl = elm
+
+                if not lbl:
+                    raise ValueError(f"Row label {row_label} not found in index")
+
+                matching_cols = []  # holds column names matching current channel
+                spans = []          # holds corresponding spanwise positions
+
+                for col in df_mean.columns:
+                    if chan in col:
+                        match = re.search(r'_n(\d)', col)
+                        node =float(col.split('_n')[-1])
+                        if match:
+                            matching_cols.append(col)
+                            spans.append(span_map[node])
+
+                if not matching_cols:
+                    raise ValueError("No valid spanwise data columns found")
+
+                # Sort columns by their spanwise numeric values
+                sorted_pairs = sorted(zip(spans, matching_cols))
+                spans, matching_cols = zip(*sorted_pairs)
+
+                # Extract mean values along span for selected row and columns
+                values = df_mean.loc[lbl, list(matching_cols)].values
+
+                ax.plot(spans, values, label=labels[j], color=colors[j], linestyle=linestyles[j])
+
+                if show_extrema and df_max is not None and df_min is not None:
+                    y_max = df_max.loc[lbl, list(matching_cols)].values
+                    y_min = df_min.loc[lbl, list(matching_cols)].values
+                    ax.scatter(spans, y_max, edgecolor=colors[j], color='none', marker='^')
+                    ax.scatter(spans, y_min, edgecolor=colors[j], color='none', marker='v')
+
+            except Exception as e:
+                print(f"Failed to plot {chan} for dataset {j}: {e}")
+                continue
+
+        ax.set_title(f"{title_prefix}{chan}")
+        ax.set_xlabel("Spanwise position (r/R)")
+        ax.set_ylabel(chan)
+        ax.grid(linestyle =':', color = 'gray')
+
+    handles, labels = axs[0].get_legend_handles_labels()
+    add_shared_legend(fig, handles, labels)
 
 def add_shared_legend(fig, handles, labels):
     fig.legend(handles, labels, loc='upper center', bbox_to_anchor=(0.5, 0.98), ncol=3, fontsize='large')
@@ -97,12 +168,13 @@ def plot_psd(ax, df, channel, fs, style, label, label_map=None):
     freqs, psd = calcPSD(signal, fs, nperseg=int(len(signal)/2), noverlap=0.5)
 
     ax.plot(freqs, psd, label=label, **style, linewidth=1)
-    ax.set_xlim([0.0, 0.5])
+    ax.set_xlim([0.0, 2.0])
     ax.set_ylabel(label_map.get(channel, channel) if label_map else channel)
     ax.tick_params(axis='both', labelsize='large')
     ax.grid(linestyle=':', color='gray')
     ax.yaxis.set_major_formatter(ScalarFormatter())
     ax.yaxis.get_major_formatter().set_powerlimits((3, 4))
+    
 
 def plot_fft(ax, df, channel, fs, style, label, label_map=None):
     
@@ -129,6 +201,7 @@ def plot_fft(ax, df, channel, fs, style, label, label_map=None):
     ax.grid(linestyle=':', color='gray')
     ax.yaxis.set_major_formatter(ScalarFormatter())
     ax.yaxis.get_major_formatter().set_powerlimits((3, 4))
+    
 
 def plot_tss(ax, df, channel, style, label, label_map=None):
     
@@ -137,3 +210,4 @@ def plot_tss(ax, df, channel, style, label, label_map=None):
     ax.grid(linestyle=':', color='gray')
     ax.yaxis.set_major_formatter(ScalarFormatter())
     ax.yaxis.get_major_formatter().set_powerlimits((0, 3))
+    
